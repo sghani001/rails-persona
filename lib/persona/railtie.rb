6bbe3ae5dd2993f55ad8ba93e1e285b705e0351f
Syed Ghani <devsyed.ghani@gmail.com>
2026-06-01 14:14:07 +0500

    fix: correct File.expand_path depth in Railtie
    ../../../ was going above gem root for both rake task and migrations path.
    ../../ is correct relative to __dir__ (lib/persona/).
    
    Fixes #5

--- lib/persona/railtie.rb
+++ lib/persona/railtie.rb
@@ -4,13 +4,13 @@ module Persona
   class Railtie < Rails::Railtie
     initializer "persona.append_migrations" do |app|
       unless app.root.to_s == File.expand_path("../..", __dir__)
-        migrations_path = File.expand_path("../../../db/migrate", __dir__)
+        migrations_path = File.expand_path("../../db/migrate", __dir__)
         app.config.paths["db/migrate"] << migrations_path
       end
     end
 
     rake_tasks do
-      load File.expand_path("../../../tasks/persona_tasks.rake", __dir__)
+      load File.expand_path("../../tasks/persona_tasks.rake", __dir__)
     end
   end
 end
